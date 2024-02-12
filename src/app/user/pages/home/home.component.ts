ab915e4eb181c30cd53076db7470028dc6f4ab0b
RaheelRiyaz <132251877+RaheelRiyaz@users.noreply.github.com>
2024-02-12 12:02:06 +0530

    Minor changes

--- src/app/user/pages/home/home.component.ts
+++ src/app/user/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
- 
+  ngOnInit() {
+    initFlowbite();
+  }
 }
