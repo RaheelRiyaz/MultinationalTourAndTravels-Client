aeb41750dff8ffea21c7c81793b4ad40d8205024
RaheelRiyaz <132251877+RaheelRiyaz@users.noreply.github.com>
2024-04-20 11:51:13 +0530

    Setting environmnet variable to local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   API_URL: 'https://api.multinationaltravellers.com/api/',
+//   IMAGE_URL: 'https://api.multinationaltravellers.com',
+//   TOKEN_Name:'multinationaltourandtravels_credentials'
+// };
+
 export const environment = {
-  API_URL: 'https://api.multinationaltravellers.com/api/',
-  IMAGE_URL: 'https://api.multinationaltravellers.com',
+  API_URL: 'http://localhost:5243/api/',
+  IMAGE_URL: 'http://localhost:5243/',
   TOKEN_Name:'multinationaltourandtravels_credentials'
 };
